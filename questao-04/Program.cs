4b6f09b92ce3a52dd69be8625e13d939d544cf9b
Emmanuel Uchoa <emmanueluchoa@yahoo.com>
2026-01-20 21:15:21 -0300

    Adicionando questão 4 e 5

---
+++ questao-04/Program.cs
@@ -0,0 +1,43 @@
+﻿Dictionary<string , int> itemsLoja = new Dictionary<string, int> {{"Mouse", 10},{"Teclado", 6}, {"Monitor", 4}, {"Cabo HDMI", 18}, {"Cadeira", 5}};
+
+Console.WriteLine("=== ESTOQUE INICIAL ===");
+foreach (var item in itemsLoja)
+{
+    Console.WriteLine($"Item: {item.Key} | Quantidade: {item.Value}");
+}
+Console.WriteLine();
+Console.WriteLine("=== OPERAÇÕES ===");
+
+itemsLoja.Add("SSD", 7);
+Console.WriteLine($"Adicionando: SSD ({itemsLoja["SSD"]})");
+
+itemsLoja["Teclado"] = 12;
+Console.WriteLine($"Atualizando: Teclado ({itemsLoja["Teclado"]})");
+
+bool existeWebcam = itemsLoja.ContainsKey("Webcam");
+if (existeWebcam)
+    Console.WriteLine("Existe Webcam? Sim");
+else
+    Console.WriteLine("Existe Webcam? Não");
+
+int contadorEstoqueBaixo = 0;
+foreach (var quantidade in itemsLoja)
+{
+    if (quantidade.Value < 8)
+    contadorEstoqueBaixo++;
+}
+Console.WriteLine($"Estoque baixo (<8): {contadorEstoqueBaixo}");
+
+itemsLoja.Remove("Monitor");
+Console.WriteLine($"Removido: Monitor");
+Console.WriteLine();
+Console.WriteLine("=== ESTOQUE FINAL ===");
+foreach (var item in itemsLoja)
+{
+    Console.WriteLine($"Item: {item.Key} | Quantidade: {item.Value}");
+}
+Console.WriteLine();
+int totalProdutos = 0;
+foreach (var quantidade in itemsLoja.Values)
+    totalProdutos += quantidade;
+Console.WriteLine($"Total de produtos: {totalProdutos}");
---
+++ questao-05/Program.cs
@@ -0,0 +1,108 @@
+﻿Console.Write("Digite o nome do cliente: ");
+string nomeCliente = Console.ReadLine();
+
+decimal valorCompra;
+Console.Write("Digite o valor da compra: R$ ");
+while (!decimal.TryParse(Console.ReadLine(), out valorCompra) || valorCompra < 0)
+    Console.WriteLine("Valor inválido");
+
+bool exit = true;
+decimal desconto = 0;
+while (exit)
+{
+    Console.Write("Tem cupom de desconto (Sim/Não): ");
+    string temCupom = Console.ReadLine().ToLower();
+    bool cupomAplicado;
+    
+    switch (temCupom)
+    {
+        case "sim":
+            cupomAplicado = true;
+            desconto = valorCompra * 0.1m;
+            exit = false;
+
+            break;
+
+        case "não":
+            cupomAplicado = false;
+            exit = false;
+            break;
+
+        default:
+            Console.WriteLine("Entrada inválida. Tente novamente. ");
+            break;
+    }
+}
+decimal valorComCupom = valorCompra - desconto;
+
+Console.WriteLine("Escolha a forma de pagamento: ");
+Console.WriteLine("1 - Dinheiro ");
+Console.WriteLine("2 - Pix");
+Console.WriteLine("3 - Débito");
+Console.WriteLine("4 - Crédito");
+Console.WriteLine("Digite 1 a 4");
+
+string numeropagamento = Console.ReadLine();
+string formaPagamento = "" ;
+decimal ajustePagamento = 0;
+string pagamento = "";
+int parcelamento = 0;
+bool loop = true;
+while (loop)
+{
+
+    switch (numeropagamento)
+    {
+        case "1":
+            formaPagamento = "Dinheiro";
+            ajustePagamento = valorComCupom * 0.05m;
+            pagamento = "Pagamento à vista";
+            loop = false;
+            break;
+
+        case "2":
+            formaPagamento = "Pix";
+            ajustePagamento = valorComCupom * 0.03m;
+            pagamento = "Pagamento à vista";
+            loop = false;
+            break;
+
+        case "3":
+            formaPagamento = "Débito";
+            pagamento = "Pagamento à vista";
+            loop = false;
+            break;
+
+        case "4":
+            formaPagamento = "Crédito";
+            ajustePagamento = valorComCupom * (-0.05m);
+            Console.Write("Quantas parcelas: ");
+            while(!int.TryParse(Console.ReadLine(), out parcelamento))
+                Console.WriteLine("Entrada Inválida");
+            pagamento = "Parcelado";
+            loop = false;
+            break;
+
+        default:
+            Console.WriteLine("Entrada inválida");
+            break;
+    }
+}
+decimal valorFinal = valorComCupom - ajustePagamento;
+decimal parcelas = 0;
+Console.Clear();
+Console.WriteLine($"=== COMPRA ONLINE ===");
+Console.WriteLine($"Cliente: {nomeCliente}");
+Console.WriteLine();
+Console.WriteLine($"CALCULO");
+Console.WriteLine($"Valor Original: {valorCompra}");
+Console.WriteLine($"Cupom: {desconto:C}");
+Console.WriteLine($"Ajuste Pagamento: {ajustePagamento:C} ");
+Console.WriteLine($"Pagamento: {formaPagamento}");
+Console.WriteLine($"Tipo: {pagamento}");
+if (pagamento == "Parcelado")
+{
+    parcelas = valorFinal/parcelamento;
+    Console.WriteLine($"Parcela: {parcelas:C}");
+}
+
